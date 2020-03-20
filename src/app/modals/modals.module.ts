deff21ed0e4ee6bb0e5b271e41b51d4441c857d1
Klavdija <klavdijaveselko96@gmail.com>
2020-03-20 15:11:34 +0100

    modals prepared

---
+++ src/app/modals/modals.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+
+// Kar rabiš v modalu, dodaš v import
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatDialogModule]
+})
+export class ModalsModule {}
