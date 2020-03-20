ac1f4ec83c38663fd2aac3cbc8fe6dbd6f712b65
Klavdija <klavdijaveselko96@gmail.com>
2020-03-20 15:02:02 +0100

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading: boolean;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    });
   }
 
+  onLogin() {
+    if (this.form.invalid || this.loading) return;
+    this.loading = true;
+    this.authService.login(this.form.value).subscribe(
+      () => {
+        this.router.navigate(["/"]);
+      },
+      err => {
+        //TODO: Handle errors
+        this.loading = false;
+      }
+    );
+  }
 }
